d9e87462e04abe5f9058cd2c45f952c2b47a42fe
Peissler <diego-peissler@hotmail.com>
2018-09-29 12:41:17 -0300

    estamos quase la

--- src/js/install.js
+++ src/js/install.js
@@ -44,12 +44,3 @@ window.addEventListener('beforeinstallprompt', function(e) {
     });
   });
 
-  window.addEventListener('beforeinstallprompt', function(e) {
-    console.log('beforeinstallprompt Event fired');
-    e.preventDefault();
-  
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-  
-    return false;
-  });
