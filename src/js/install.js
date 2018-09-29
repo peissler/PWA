aea1c23a14071c27859171dc5062f0052a18abb3
Peissler <diego-peissler@hotmail.com>
2018-09-29 12:18:52 -0300

    mais uma vez

--- src/js/install.js
+++ src/js/install.js
@@ -1,6 +1,32 @@
 var deferredPrompt;
 var installButton = document.getElementById('install');
 
+installButton.addEventListener('click', function(e) {
+    console.log('show');
+    if(deferredPrompt !== undefined) {
+      // The user has had a postive interaction with our app and Chrome
+      // has tried to prompt previously, so let's show the prompt.
+      deferredPrompt.prompt();
+  
+      // Follow what the user has done with the prompt.
+      deferredPrompt.userChoice.then(function(choiceResult) {
+  
+        console.log(choiceResult.outcome);
+  
+        if(choiceResult.outcome == 'dismissed') {
+          console.log('User cancelled home screen install');
+        }
+        else {
+          console.log('User added to home screen');
+        }
+  
+        // We no longer need the prompt.  Clear it up.
+        deferredPrompt = null;
+      });
+    }
+  });
+
+
 window.addEventListener('beforeinstallprompt', function(e) {
     // beforeinstallprompt Event fired
   
@@ -27,30 +53,4 @@ window.addEventListener('beforeinstallprompt', function(e) {
     deferredPrompt = e;
   
     return false;
-  });
- 
-  
-  installButton.addEventListener('click', function(e) {
-    console.log('show');
-    if(deferredPrompt !== undefined) {
-      // The user has had a postive interaction with our app and Chrome
-      // has tried to prompt previously, so let's show the prompt.
-      deferredPrompt.prompt();
-  
-      // Follow what the user has done with the prompt.
-      deferredPrompt.userChoice.then(function(choiceResult) {
-  
-        console.log(choiceResult.outcome);
-  
-        if(choiceResult.outcome == 'dismissed') {
-          console.log('User cancelled home screen install');
-        }
-        else {
-          console.log('User added to home screen');
-        }
-  
-        // We no longer need the prompt.  Clear it up.
-        deferredPrompt = null;
-      });
-    }
   });
