c27ac91f71d3929ed67317c6ed81675d4222b837
Peissler <diego-peissler@hotmail.com>
2018-09-29 12:25:19 -0300

    Teste

--- src/js/feed.js
+++ src/js/feed.js
@@ -22,27 +22,6 @@ button.addEventListener('click', function (e) {
 
 var installButton = document.getElementById('botaoinstalar');
 
-installButton.addEventListener('click', function(e) {
-    console.log('show');
-    if(deferredPrompt !== undefined) {
-      // The user has had a postive interaction with our app and Chrome
-      // has tried to prompt previously, so let's show the prompt.
-      deferredPrompt.prompt();
-  
-      // Follow what the user has done with the prompt.
-      deferredPrompt.userChoice.then(function(choiceResult) {
-  
-        console.log(choiceResult.outcome);
-  
-        if(choiceResult.outcome == 'dismissed') {
-          console.log('User cancelled home screen install');
-        }
-        else {
-          console.log('User added to home screen');
-        }
-  
-        // We no longer need the prompt.  Clear it up.
-        deferredPrompt = null;
-      });
-    }
-  });
+installButton.addEventListener("click", function(e) {
+    console.log('GOOOO!');
+})
